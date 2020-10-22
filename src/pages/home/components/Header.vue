9ac571b1be04a600fe2a3977adf4bf3a30f04602
barbara <289300388@qq.com>
2020-10-22 10:27:03 +0800

    initialization

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <div class="header">
+    <div class="header-left">1</div>
+    <div class="header-input"></div>
+    <div class="header-left"></div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+}
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
