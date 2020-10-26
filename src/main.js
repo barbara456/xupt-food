3493949cbca7abed98b732456dbe08343fd9ce2b
barbara <289300388@qq.com>
2020-10-26 11:47:34 +0800

    use data loop to display the carousel

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import router from './router'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data(){
+    return{
+      swiperOptions: {
+        pagination:'.swiper-pagination',
+        loop:true
+      },
+      swiperList:[{
+        id:'0001',
+        imgUrl:'http://www.xiyou.edu.cn/images/0929guoqing.jpg'
+      },{
+        id:'0002',
+        imgUrl:'http://www.xiyou.edu.cn/images/0905jieyueliangshi.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background #fff
+.wrapper
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 27.44%
+  .swiper-img
+    width 100%
+</style>
